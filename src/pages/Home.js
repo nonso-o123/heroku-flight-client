39126cbd7ad136c5d53732f13f870f9ad5c0a8f1
Chinonso Obidike <gloryshines4ever@yahoo.com>
2021-09-09 14:48:08 -0400

    changed travel-payouts credentials

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,7 +18,7 @@ export default function Home({
     return (
         <div className="container-fluid">
             <Banner bannerStyle="home-banner">
-                <iframe title="search-form" src="//www.travelpayouts.com/widgets/a5e0119e48a84844dd66a80aa3027844.html?v=2210"></iframe>
+                <iframe title="search-form" scrolling="auto" className="tp-form" frameBorder="0" src="//www.travelpayouts.com/widgets/a5e0119e48a84844dd66a80aa3027844.html?v=2210"></iframe>
             </Banner>
             {modalOpen && <Overlay innerRef={containerRef} overlayOpen={modalOpen} setOverlayOpen={setModalOpen}>
                 <section className="clearfix position-absolute signup-section" ref={containerRef}>
